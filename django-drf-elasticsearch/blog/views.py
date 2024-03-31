d7a051dd9c93008fbfbf235b8690f9804a04ff58
El amrani Amine <amine_el_amrani@outlook.com>
2024-03-31 14:11:15 +0200

    Add viewsets for User, Category, and Article

--- django-drf-elasticsearch/blog/views.py
+++ django-drf-elasticsearch/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from blog.models import Article, Category, User
+from blog.serializers import ArticleSerializer, CategorySerializer, UserSerializer
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
